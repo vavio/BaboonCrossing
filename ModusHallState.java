3c1b3dd056f20e8691287496185391af78b87a06
Valentin Ambaroski <vavio@users.noreply.github.com>
2014-09-30 22:56:14 +0200

    2nd version
    Change error generating functions

--- ModusHallState.java
+++ ModusHallState.java
@@ -8,19 +8,17 @@ public class ModusHallState extends AbstractState {
 		NEUTRAL, HEATHENS_RULE, PRUDES_RULE, TRANSITION_TO_HEATHENS, TRANSITION_TO_PRUDES
 	};
 
-	private static final String HEATHENS_CANT_PASS = "The current state is PRUDES_RULES or TRANSITION_TO_PRUDES or NEUTRAL";
+	private static final String HEATHENS_CANT_PASS = "The current state is PRUDES_RULES or TRANSITION_TO_PRUDES";
 	private static final String PRUDES_CANT_PASS = "The current state is HEATHENS_RULES or TRANSITION_TO_HEATHENS";
-	
+
 	private static final int HEATHENS_CANT_PASS_POINTS = 15;
 	private static final int PRUDES_CANT_PASS_POINTS = 15;
 
 	private static final int WRONG_STATE_POINTS = 10;
 
 	private static final String NOT_PARALLEL = "The crossing is not in parallel!";
-;
+
 	private static final int NOT_PARALLEL_POINTS = 5;
-
-
 
 	StatusState state;
 
@@ -30,7 +28,7 @@ public class ModusHallState extends AbstractState {
 	private BoundCounterWithRaceConditionCheck heathensWaiting;
 
 	public ModusHallState() {
-		prudesWaiting = new BoundCounterWithRaceConditionCheck(0, null, 0, null, 0, 10, "Less than points"); 
+		prudesWaiting = new BoundCounterWithRaceConditionCheck(0);
 		prudesWaiting = new BoundCounterWithRaceConditionCheck(0);
 		prudesPassed = new BoundCounterWithRaceConditionCheck(0);
 		heathensPassed = new BoundCounterWithRaceConditionCheck(0);
@@ -39,8 +37,10 @@ public class ModusHallState extends AbstractState {
 	}
 
 	public void setNeutral() {
+		Switcher.forceSwitch(5);
 		synchronized (this) {
-			if (prudesWaiting.getValue() == 0 && heathensWaiting.getValue() == 0)
+			if (prudesWaiting.getValue() == 0
+					&& heathensWaiting.getValue() == 0)
 				state = StatusState.NEUTRAL;
 			else
 				new PointsException(WRONG_STATE_POINTS, ErrorMessageNeutral());
@@ -48,18 +48,16 @@ public class ModusHallState extends AbstractState {
 	}
 
 	private String ErrorMessageNeutral() {
-		return String.format(
-				"NEUTRAL state can not be set with %d prudes and %d heathens.",
-				prudesWaiting, heathensWaiting);
+		return "NEUTRAL state can not be set with " + prudesWaiting.getValue()
+				+ " prudes and " + heathensWaiting.getValue() + " heathens.";
 	}
 
 	public void setHeathensRule() {
 		synchronized (this) {
-			if (getState() == StatusState.NEUTRAL && prudesWaiting.getValue() == 0)
+			if (getState() == StatusState.NEUTRAL
+					&& prudesWaiting.getValue() == 0)
 				state = StatusState.HEATHENS_RULE;
-			else
-				new PointsException(WRONG_STATE_POINTS,
-						ErrorMessageHeathensRules());
+			
 		}
 	}
 
@@ -67,8 +65,8 @@ public class ModusHallState extends AbstractState {
 		if (prudesWaiting.getValue() == 0)
 			return "To set HEATHENS_RULE the previous state must be NEUTRAL";
 		else
-			return String.format("Can not set HEATHENS_RULE with %d prudes waiting to pass",
-					prudesWaiting);
+			return "Can not set HEATHENS_RULE with " + prudesWaiting.getValue()
+					+ " prudes waiting to pass";
 	}
 
 	public void setTransitionHeathens() {
@@ -76,9 +74,7 @@ public class ModusHallState extends AbstractState {
 			if (state == StatusState.PRUDES_RULE
 					&& heathensWaiting.getValue() > prudesWaiting.getValue())
 				state = StatusState.TRANSITION_TO_HEATHENS;
-			else
-				new PointsException(WRONG_STATE_POINTS,
-						ErrorMessageTransitionHeathens());
+
 		}
 	}
 
@@ -93,11 +89,10 @@ public class ModusHallState extends AbstractState {
 
 	public void setPrudesRule() {
 		synchronized (this) {
-			if (getState() == StatusState.NEUTRAL && heathensWaiting.getValue() == 0)
+			if (getState() == StatusState.NEUTRAL
+					&& heathensWaiting.getValue() == 0)
 				state = StatusState.PRUDES_RULE;
-			else
-				new PointsException(WRONG_STATE_POINTS,
-						ErrorMessagePrudesRules());
+
 		}
 	}
 
@@ -105,8 +100,8 @@ public class ModusHallState extends AbstractState {
 		if (prudesWaiting.getValue() == 0)
 			return "To set PRUDES_RULE the previous state must be NEUTRAL";
 		else
-			return String.format("Can not set PRUDES_RULE with %d heathens waiting to pass",
-					heathensWaiting);
+			return "Can not set PRUDES_RULE with " + heathensWaiting.getValue()
+					+ " heathens waiting to pass";
 	}
 
 	public void setTransitionPrudes() {
@@ -114,9 +109,7 @@ public class ModusHallState extends AbstractState {
 			if (state == StatusState.HEATHENS_RULE
 					&& prudesWaiting.getValue() > heathensWaiting.getValue())
 				state = StatusState.TRANSITION_TO_PRUDES;
-			else
-				new PointsException(WRONG_STATE_POINTS,
-						ErrorMessageTransitionPrudes());
+
 		}
 	}
 
@@ -124,44 +117,57 @@ public class ModusHallState extends AbstractState {
 		if (state != StatusState.PRUDES_RULE)
 			return "To set TRANSITION_TO_PRUDES the previous state must be HEATHEN_RULES";
 		else
-			return String
-					.format("#Heathens must be smaller than #Prudes. Current state: Heathens: %d - Prudes: %d",
-							heathensWaiting, prudesWaiting);
+			return "#Heathens must be smaller than #Prudes. Current state: Heathens: "
+					+ heathensWaiting + " - Prudes: " + prudesWaiting;
 	}
 
 	public StatusState getState() {
 		return state;
 	}
 
-	public void crossPrude()
-	{
+	public void crossPrude() {
+		Switcher.forceSwitch(5);
 		synchronized (this) {
-			if (state == StatusState.TRANSITION_TO_PRUDES)
-			{
-				log(prudesWaiting.decrementWithMin(false), "Prude is leaving the turnsteel");
-				log(prudesPassed.incrementWithMax(false), "Prude is crossing the field");
-			}
-			else
-				throw new PointsException(PRUDES_CANT_PASS_POINTS, PRUDES_CANT_PASS);
+			if (state == StatusState.TRANSITION_TO_PRUDES
+					|| state == StatusState.PRUDES_RULE) {
+				log(prudesWaiting.decrementWithMin(false),
+						"Prude is leaving the turnsteel");
+				log(prudesPassed.incrementWithMax(false),
+						"Prude is crossing the field");
+			} 
 		}
 	}
-	
-	public void crossHeathen()
-	{
-		synchronized (this) {
-			if (state == StatusState.TRANSITION_TO_PRUDES)
-			{
-				log(heathensWaiting.decrementWithMin(false), "Heathen is leaving the turnsteel");
-				log(heathensPassed.incrementWithMax(false), "Heathen is crossing the field");
-			}
-			else
-				throw new PointsException(HEATHENS_CANT_PASS_POINTS, HEATHENS_CANT_PASS);
-		}
-	}
-	
-	
 
-	private synchronized void reset() {
+	public void crossHeathen() {
+		Switcher.forceSwitch(5);
+		synchronized (this) {
+			if (state == StatusState.TRANSITION_TO_HEATHENS
+					|| state == StatusState.HEATHENS_RULE) {
+				log(heathensWaiting.decrementWithMin(false),
+						"Heathen is leaving the turnsteel");
+				log(heathensPassed.incrementWithMax(false),
+						"Heathen is crossing the field");
+			}
+			}
+	}
+
+	public void enterPrude() {
+		Switcher.forceSwitch(5);
+		synchronized (this) {
+			log(prudesWaiting.incrementWithMax(false),
+					"Prude is passing the turnsteel");
+		}
+	}
+
+	public void enterHeathen() {
+		Switcher.forceSwitch(5);
+		synchronized (this) {
+			log(heathensWaiting.incrementWithMax(false),
+					"Heathen is passing the turnsteel");
+		}
+	}
+
+	public synchronized void reset() {
 		prudesWaiting.setValue(0);
 		prudesPassed.setValue(0);
 		heathensWaiting.setValue(0);
